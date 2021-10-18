28269b6af0fe9d7587038478a549084b8a4d170b
Alessandra Boccuto <aless97b@gmail.com>
2021-10-18 21:57:46 +0200

    add two different data mode

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+from logging import error
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -10,22 +11,37 @@ class Dataset:
     x_test = None
     t_test = None
 
-    def __init__(self, csv_path):
+    def load_data(self, csv_path):
+        ''' Load general data from csv'''
         self.data = pd.read_csv(csv_path)
+    
+    def load_train_test_data(self, csv_train, csv_test):
+        ''' Load train and test data fomr specific csv'''
+        train_data = pd.read_csv(csv_train)
+        self.x_train = train_data[[train_data.columns.drop('noise')]]
+        self.y_train = train_data['noise']
+        
+        test_data = pd.read_csv(csv_test)
+        self.x_test = test_data[[test_data.columns.drop('noise')]]
+        self.y_test = test_data['noise']
+
 
     def split(self, test_size, random_state):
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
-        self.data[['longitude', 'latitude']],
+        self.data[self.data.columns.drop('noise')], # all columns except noise
         self.data['noise'],
         test_size=test_size,
         random_state=random_state)
-    
+
     def add_column_to_train(self, name, values):
         self.x_train[name] = values
 
     def train_to_csv(self, path):
         to_save = self.x_train.join(self.y_train)
-        to_save.to_csv(path)
+        to_save.to_csv(path, index = False)
 
+    def test_to_csv(self, path):
+        to_save = self.x_test.join(self.y_test)
+        to_save.to_csv(path, index = False)
 
         
