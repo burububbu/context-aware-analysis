e3e9e93cda0332f01082223c3f9385f0cea72c29
Alessandra Boccuto <aless97b@gmail.com>
2021-11-10 14:33:55 +0100

    added training progress

--- utils/nn_utils.py
+++ utils/nn_utils.py
@@ -9,41 +9,52 @@ import torch
 import utils.utils as utils
 import itertools
 
+
 def train_neural_networks(train_dataset, test_dataset, params):
     torch.manual_seed(42)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    # generate all possible combinations 
-    hyperparams = itertools.product(params['hidden_sizes'], params['nums_layers'], params['num_epochs'], params['batch_sizes'], params['learning_rates'])
+    # generate all possible combinations
+    hyperparams = itertools.product(params['hidden_sizes'], params['nums_layers'],
+                                    params['num_epochs'], params['batch_sizes'], params['learning_rates'])
+
+    num_combinations = len(params['hidden_sizes']) * len(params['nums_layers']) * len(
+        params['num_epochs']) * len(params['batch_sizes']) * len(params['learning_rates'])
 
     loss = nn.MSELoss()
 
     results = []
 
     # hyperparams tuning
-    for hidden_size, num_layers, num_epoch, batch_size, learning_rate in hyperparams:
-        print(f'\t\tTraining model with {hidden_size} hidden size, {num_layers+1 } layers, {num_epoch} epochs, {learning_rate} learning rate and {batch_size} as batch size')
+    for i, (hidden_size, num_layers, num_epoch, batch_size, learning_rate) in enumerate(hyperparams):
+        print(
+            f'\t\t{i+1} / {num_combinations} ({round((i+1)/num_combinations*100,3)}%)')
+        print(
+            f'\t\tTraining model with {hidden_size} hidden size, {num_layers+1 } layers, {num_epoch} epochs, {learning_rate} learning rate and {batch_size} as batch size')
 
         # create model
-        model = NeuralNet(train_dataset.X.shape[1], hidden_size, num_layers).to(device)
+        model = NeuralNet(
+            train_dataset.X.shape[1], hidden_size, num_layers).to(device)
 
         optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-        
+
         # create Dataloader
         train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
         test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 
         mse_train = None
 
-        #train
+        # train
         for _ in range(num_epoch):
-            mse_train = _train(train_dataloader, model, loss, optimizer, device) # then save last mse
+            mse_train = _train(train_dataloader, model, loss,
+                               optimizer, device)  # then save last mse
 
         # test
         mse_test = _test(test_dataloader, model, loss, device)
-        
-        r2_train, rae_train = _compute_statistics(train_dataloader, model, device)
+
+        r2_train, rae_train = _compute_statistics(
+            train_dataloader, model, device)
         r2_test, rae_test = _compute_statistics(test_dataloader, model, device)
 
         rmse_train = sqrt(mse_train)
@@ -61,31 +72,34 @@ def train_neural_networks(train_dataset, test_dataset, params):
             'num_epochs': num_epoch,
             'batch_size': batch_size,
             'lr': learning_rate
-            }
+        }
 
-        results.append([params, r2_train, r2_test, mse_train, mse_test, rmse_train, rmse_test, rae_train, rae_test])
+        results.append([params, r2_train, r2_test, mse_train,
+                       mse_test, rmse_train, rmse_test, rae_train, rae_test])
 
     return results
-        
+
+
 def _compute_statistics(dataloader, model, device):
     model.eval()
 
     with torch.no_grad():
         x_data = dataloader.dataset.X.to(device)
 
-        preds = model(x_data) # all data
+        preds = model(x_data)  # all data
         preds = preds.detach().cpu().numpy()
 
         y_data = dataloader.dataset.y.detach().cpu().numpy()
-        
+
         r2 = r2_score(y_data, preds)
         rae = utils.rae(y_data, preds)
 
     return r2, rae
 
+
 def _train(dataloader, model, loss_fn, optimizer, device):
-    model.train() # set model model
-    
+    model.train()  # set model model
+
     num_batches = len(dataloader)
     total_loss = 0
 
@@ -105,9 +119,10 @@ def _train(dataloader, model, loss_fn, optimizer, device):
 
     return total_loss/num_batches
 
+
 def _test(dataloader, model, loss_fn, device):
     model.eval()
-    
+
     num_batches = len(dataloader)
     total_loss = 0
 
@@ -116,7 +131,7 @@ def _test(dataloader, model, loss_fn, device):
             data, targets = data.to(device), targets.to(device)
 
             prediction = model(data)
-            loss =loss_fn(prediction.flatten(), targets)
+            loss = loss_fn(prediction.flatten(), targets)
             total_loss += loss.item()
 
     return total_loss/num_batches
