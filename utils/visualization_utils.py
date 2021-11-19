707fb0deb8dd7f40b5f6fd4b0005773c6863fdbb
Alessandra Boccuto <aless97b@gmail.com>
2021-11-19 13:15:54 +0100

    moved plot functions and print best params

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-def plot_loss(losses, params, loss_type, set_type='', prepr_type=''):
+
+def plot_loss(losses, params, loss_type, set_type='', prepr_type='', to_save=True):
+    plt.clf()
     plt.plot(list(range(params['n_epochs'])), losses)
 
     plt.ylabel("mse loss")
@@ -17,7 +21,38 @@ def plot_loss(losses, params, loss_type, set_type='', prepr_type=''):
 
     plt.title(f"{params_to_string} training {loss_type}")
 
-    save(f"./plots/{set_type}_{prepr_type}_losses_{params_to_string}_.png")
+    if to_save:
+        save(f"./plots/{set_type}_{prepr_type}_losses_{params_to_string}_.png")
+
+
+# init useful functions
+def grouped_bar_plot(x, y, hue, data, model_name, legend_pos='lower right'):
+    # plt.figure(dpi=dpi)
+
+    sns.set_style(style='darkgrid')
+    sns.barplot(x=x, y=y, hue=hue, data=data)
+    plt.legend(loc=legend_pos)
+
+    plt.ylabel(f"{y.replace('_', ' ')} value")
+    plt.xlabel(x.replace('_', ' '))
+    plt.title(f"{model_name} scores")
+
+
+def train_test_bar_plot(train_values, test_values, groups, score_type, title):
+    fig = plt.figure()
+    X = np.arange(0, len(groups)/2, 0.5)
+
+    ax = fig.add_axes([0, 0, 1, 1])
+    ax.bar(X + 0.00, train_values, width=0.20)
+    ax.bar(X + 0.20, test_values, width=0.20)
+    plt.xticks(X + 0.10, groups)
+
+    ax.legend(labels=['Train', 'Test'])
+    plt.xlabel('Type')
+    plt.ylabel(score_type)
+
+    plt.title(title)
+
 
 def save(title):
     plt.savefig(title)
