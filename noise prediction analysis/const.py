7e68d3ac7eea84595dd9b3be75866a7bca372508
Alessandra Boccuto <aless97b@gmail.com>
2022-02-11 16:06:03 +0100

    load const file

---
+++ noise prediction analysis/const.py
@@ -0,0 +1,10 @@
+csv_data = "./data/noises.csv"
+csv_train_data = "./data/train_data.csv"
+csv_test_data = "./data/test_data.csv"
+
+csv_results_folder = "./results/"
+
+
+test_size = 0.20
+random_seed = 42
+learners_neighbors = [5, 10, 50]
